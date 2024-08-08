da1c7660efbf7f7ed0cfc5d31988be6d7a30693c
Freedy-R <dedspinpolski@gmail.com>
2024-08-08 18:53:33 +0200

    fix nav

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -8,19 +8,22 @@ const GET_RESOURCES = gql`
   }
 `;
 const Resources = () => {
-  const { data, loading, error } = useQuery(GET_RESOURCES);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Not loaded</p>;
+  // const { data, loading, error } = useQuery(GET_RESOURCES);
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Not loaded</p>;
   return (
     <>
-      <section className="resource">
+      {/* <section className="resource">
         {data.resources.map((resource) => (
           <div className="resource_item" key={resource.id}>
             {resource.name}
             <button className="btn_resource_reserve">Reserve</button>
           </div>
         ))}
-      </section>
+      </section> */}
+      <main className="room_main">
+        <section className="resource"></section>
+      </main>
     </>
   );
 };
